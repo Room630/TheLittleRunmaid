32022457f133983e76e688a44757a9eaec02e817
변혜빈 <seramirochia@gmail.com>
2022-05-12 02:26:02 +0900

    플레이어 움직임
    버튼을 누르면 플레이어가 y방향으로 이동한다.

---
+++ TheAdventuresOfTheMermaid/Assets/Scripts/PlayerStat.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+    public int maxHP; // 하트의 개수, 체력
+    public int coin; // q,보
+    public int star;
+    public char[] hadItem = new char[3]; // 소지중인 아이템
+// 현재 소유중인 소모성 아이템. 상점에서 최대 3개 구입해서 소지 가능
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
